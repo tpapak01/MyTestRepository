62262a0b954227032cce9c41ec1263bf6e292e88
tpapak01 <tpapak01@cs.ucy.ac.cy>
2022-05-04 12:29:45 +0300

    Commit of different branch! Not master branch!

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def print_hi(name):
 
     print("More stuff")
 
+    print("Stuff that only exists in myPersonalBranch!")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
